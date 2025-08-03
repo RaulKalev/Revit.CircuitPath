195d9625bc7bdf7bf67158a383dbc32e0c19ae54
Raul Kalev <mibilit@me.com>
2025-08-03 15:42:36 +0300

    Add project files.

---
+++ CircuitPath/App.cs
@@ -0,0 +1,40 @@
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using ricaun.Revit.UI;
+using System;
+
+namespace CircuitPath
+{
+    [AppLoader]
+    public class App : IExternalApplication
+    {
+        private RibbonPanel ribbonPanel;
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            const string tabName = "RK Tools";
+            const string panelName = "Tools";
+
+            try { application.CreateRibbonTab(tabName); } catch { }
+            ribbonPanel = application.CreateOrSelectPanel(tabName, panelName);
+
+            // Create the buttons
+            var btn1 = ribbonPanel.CreatePushButton<Command>("Set circuit path")
+                .SetLargeImage("Resources/AllDevices.tiff")
+                .SetToolTip("Set circuit path mode to \"All Devices\" for all circuits in the project.")
+                .SetContextualHelp("https://raulkalev.github.io/rktools/");
+
+
+            // Stack them (they will appear as small buttons in a row)
+            ribbonPanel.RowStackedItems(btn1);
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            ribbonPanel?.Remove();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ CircuitPath/Command.cs
@@ -0,0 +1,51 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.UI;
+using System.Linq;
+
+namespace CircuitPath
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elementSet)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var circuits = new FilteredElementCollector(doc)
+                .OfClass(typeof(ElectricalSystem))
+                .Cast<ElectricalSystem>()
+                .ToList();
+
+            if (circuits.Count == 0)
+            {
+                TaskDialog.Show("Set All Circuits", "No electrical circuits found.");
+                return Result.Succeeded;
+            }
+
+            int setCount = 0;
+
+            using (Transaction tx = new Transaction(doc, "Set Circuits Path Mode to All Devices"))
+            {
+                tx.Start();
+                foreach (var circuit in circuits)
+                {
+                    // Check current value to avoid unnecessary sets
+                    if (circuit.CircuitPathMode != ElectricalCircuitPathMode.AllDevices)
+                    {
+                        circuit.CircuitPathMode = ElectricalCircuitPathMode.AllDevices;
+                        setCount++;
+                    }
+                }
+                tx.Commit();
+            }
+
+            TaskDialog.Show("Set All Circuits",
+                $"Updated {setCount} circuit(s) to Path Mode = All Devices.");
+
+            return Result.Succeeded;
+        }
+    }
+}
